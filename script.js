209511979546a2fac7cd555a4ce9d4c0b55cadc9
Himani Kotnala <himanikotnala8126@gmail.com>
2025-11-18 10:14:11 +0530

    Added skill bar

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
   navLinks.classList.toggle('active');
 });
+const skillLevels = document.querySelectorAll('.skill-level');
+
+skillLevels.forEach(skill => {
+ const width = skill.classList.contains('html') ? '90%' :
+                skill.classList.contains('css') ? '85%' :
+                skill.classList.contains('js') ? '75%' :
+                skill.classList.contains('excel') ? '70%' :
+                skill.classList.contains('sql') ? '75%' :
+                skill.classList.contains('python') ? '50%' :
+                '0';
+
+  skill.style.width = width;
+});
+
